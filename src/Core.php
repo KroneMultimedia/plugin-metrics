7b6151700e419c672e15fce40debf61df650eed3
Helmut Januschka <helmut@januschka.com>
2018-08-03 10:10:15 +0200

    up

--- src/Core.php
+++ src/Core.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace KMM\Metrics;
 
-use Monolog\Logger;
 use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Logger;
 
 class Core
 {
@@ -17,14 +17,14 @@ class Core
         $this->wpdb = $wpdb;
         $this->plugin_dir = plugin_dir_url(__FILE__) . '../';
 
-        if (!defined("KRN_METRICS_STATSD_HOST")) {
-            define("KRN_METRICS_STATSD_HOST", "stats.krn.krone.at");
+        if (! defined('KRN_METRICS_STATSD_HOST')) {
+            define('KRN_METRICS_STATSD_HOST', 'stats.krn.krone.at');
         }
-        if (!defined("KRN_HOST_SYSLOG")) {
-                define("KRN_HOST_SYSLOG", "syslog.krn.krone.at");
+        if (! defined('KRN_HOST_SYSLOG')) {
+            define('KRN_HOST_SYSLOG', 'syslog.krn.krone.at');
         }
-        if (!defined("KRN_MY_HOSTNAME")) {
-                define("KRN_MY_HOSTNAME", gethostname());
+        if (! defined('KRN_MY_HOSTNAME')) {
+            define('KRN_MY_HOSTNAME', gethostname());
         }
 
         $this->statsd = new StatsD(KRN_METRICS_STATSD_HOST);
@@ -34,48 +34,40 @@ class Core
         $this->add_metabox();
     }
 
-
-
     public function add_metabox()
     {
     }
-
 
     public function add_filters()
     {
     }
 
-
-
-
     public function add_actions()
     {
-
-        add_action('krn_send_stats', array($this, 'send_stats'), 10, 1);
+        add_action('krn_send_stats', [$this, 'send_stats'], 10, 1);
         //CloudWatch
-                //Needed for BC
-        add_action('krn_log_cloudwatch', [$this, "log_syslog"], 10, 2);
-        add_action('krn_log_syslog', [$this, "log_syslog"], 10, 2);
+        //Needed for BC
+        add_action('krn_log_cloudwatch', [$this, 'log_syslog'], 10, 2);
+        add_action('krn_log_syslog', [$this, 'log_syslog'], 10, 2);
     }
 
     //Actual Methods
     public function send_stat($key, $value, $type)
     {
-        if ($type == "counting") {
-                    $this->statsd->counting($key, $value);
+        if ($type == 'counting') {
+            $this->statsd->counting($key, $value);
         }
-        if ($type == "timing") {
-                    $this->statsd->timing($key, $value);
+        if ($type == 'timing') {
+            $this->statsd->timing($key, $value);
         }
     }
 
-    public function log_syslog($message, $level = "warning")
+    public function log_syslog($message, $level = 'warning')
     {
-
         $handler = new SyslogUdpHandler(KRN_HOST_SYSLOG, 514);
         $log = new Logger('krn.cloudwatch');
 
-        $formatter = new LineFormatter("(BACKEND-" . KRN_MY_HOSTNAME . ") %level_name%: %message%\n");
+        $formatter = new LineFormatter('(BACKEND-' . KRN_MY_HOSTNAME . ") %level_name%: %message%\n");
         $handler->setFormatter($formatter);
 
         $log->pushHandler($handler);
--- src/StatsD.php
+++ src/StatsD.php
@@ -2,38 +2,51 @@
 
 namespace KMM\Metrics;
 
-
 class StatsD
 {
-    private $host, $port, $logger;
+    private $host;
+    private $port;
+    private $logger;
+
     // Instantiate a new client
-    public function __construct($host = 'localhost', $port = 8125, $logger = null) {
+    public function __construct($host = 'localhost', $port = 8125, $logger = null)
+    {
         $this->host = $host;
         $this->port = $port;
         $this->logger = $logger;
     }
+
     // Record timing
-    public function timing($key, $time, $rate = 1) {
+    public function timing($key, $time, $rate = 1)
+    {
         $this->send("$key:$time|ms", $rate);
     }
+
     // Record gauge
-    public function gauge($key, $gauge) {
+    public function gauge($key, $gauge)
+    {
         $this->send("$key:$gauge|g");
     }
+
     // Time something
-    public function time_this($key, $callback, $rate = 1) {
+    public function time_this($key, $callback, $rate = 1)
+    {
         $begin = microtime(true);
         $callback();
         $time = floor((microtime(true) - $begin) * 1000);
         // And record
         $this->timing($key, $time, $rate);
     }
+
     // Record counting
-    public function counting($key, $amount = 1, $rate = 1) {
+    public function counting($key, $amount = 1, $rate = 1)
+    {
         $this->send("$key:$amount|c", $rate);
     }
+
     // Send
-    private function send($value, $rate = NULL) {
+    private function send($value, $rate = null)
+    {
         $fp = @fsockopen('udp://' . $this->host, $this->port, $errno, $errstr);
         // Will show warning if not opened, and return false
         if ($fp) {
@@ -42,5 +55,3 @@ class StatsD
         }
     }
 }
-
-

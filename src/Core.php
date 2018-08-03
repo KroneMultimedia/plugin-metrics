59afaf9192672e18028d3fefd077524140c5cb29
Helmut Januschka <helmut@januschka.com>
2018-08-03 10:41:17 +0200

    up

--- src/Core.php
+++ src/Core.php
@@ -49,7 +49,9 @@ class Core
         //Needed for BC
         add_action('krn_log_cloudwatch', [$this, 'log_syslog'], 10, 2);
         add_action('krn_log_syslog', [$this, 'log_syslog'], 10, 2);
-    
+
+        add_action("save_post", [$this, "save_post_start"], -1, 3);
+        add_action("save_post", [$this, "save_post_end"], 99999, 3);
     }
     //Actual Methods
     public function send_stat($key, $value, $type)
@@ -64,7 +66,8 @@ class Core
 
     public function log_syslog($message, $level = 'warning')
     {
-      var_dump(1); exit;
+        var_dump(1);
+        exit;
         $handler = new SyslogUdpHandler(KRN_HOST_SYSLOG, 514);
         $log = new Logger('krn.cloudwatch');
 
@@ -73,5 +76,18 @@ class Core
 
         $log->pushHandler($handler);
         $log->$level($message);
+    }
+    public function save_post_start($post)
+    {
+            $this->save_post_start=microtime(true);
+    }
+
+    public function save_post_end($post)
+    {
+            $type = get_post_type($post);
+            $this->save_post_end=microtime(true);
+            $ms = round($this->save_post_end-$this->save_post_start, 3)*1000;
+            do_action('krn_send_stat', 'krn.backend.save_time.all', $ms, "timing");
+            do_action('krn_send_stat', 'krn.backend.save_time.' . $type, $ms, "timing");
     }
 }

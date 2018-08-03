26ae9061b4e3edca57a29dec4aa16733b19c3d78
Helmut Januschka <helmut@januschka.com>
2018-08-03 10:05:51 +0200

    add logic

---
+++ src/StatsD.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace KMM\Metrics;
+
+
+class StatsD
+{
+    private $host, $port, $logger;
+    // Instantiate a new client
+    public function __construct($host = 'localhost', $port = 8125, $logger = null) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->logger = $logger;
+    }
+    // Record timing
+    public function timing($key, $time, $rate = 1) {
+        $this->send("$key:$time|ms", $rate);
+    }
+    // Record gauge
+    public function gauge($key, $gauge) {
+        $this->send("$key:$gauge|g");
+    }
+    // Time something
+    public function time_this($key, $callback, $rate = 1) {
+        $begin = microtime(true);
+        $callback();
+        $time = floor((microtime(true) - $begin) * 1000);
+        // And record
+        $this->timing($key, $time, $rate);
+    }
+    // Record counting
+    public function counting($key, $amount = 1, $rate = 1) {
+        $this->send("$key:$amount|c", $rate);
+    }
+    // Send
+    private function send($value, $rate = NULL) {
+        $fp = @fsockopen('udp://' . $this->host, $this->port, $errno, $errstr);
+        // Will show warning if not opened, and return false
+        if ($fp) {
+            @fwrite($fp, $rate ? "$value|@$rate" : $value);
+            @fclose($fp);
+        }
+    }
+}
+
+
